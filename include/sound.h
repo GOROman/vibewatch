cdf5217ecbe9527fe5c8665a1b531fa5ec5fd85c
GOROman <yoshihito.kondoh@gmail.com>
2026-08-08 11:10:10 +0900

    Rename sound header

---
+++ include/sound.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+
+namespace sound {
+
+enum class Effect {
+    Start,
+    StartReverse,
+};
+
+void playSquare(float frequency, std::uint32_t durationMs, std::uint8_t volume);
+void playEffect(Effect effect, std::uint8_t volume, std::uint8_t tempoMultiplier = 1);
+
+}  // namespace sound
