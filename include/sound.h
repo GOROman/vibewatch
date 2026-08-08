0b0b51ac77f897b9b28daed0425473a56245c34c
GOROman <yoshihito.kondoh@gmail.com>
2026-08-08 12:16:14 +0900

    Expose triangle-wave playback

--- include/sound.h
+++ include/sound.h
@@ -10,6 +10,7 @@ enum class Effect {
 };
 
 void playSquare(float frequency, std::uint32_t durationMs, std::uint8_t volume);
+void playTriangle(float frequency, std::uint32_t durationMs, std::uint8_t volume);
 void playEffect(Effect effect, std::uint8_t volume, std::uint8_t tempoMultiplier = 1);
 
 }  // namespace sound
